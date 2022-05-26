31bed2bc0d21a8d095bf988dd82749c1b0a46975
adan <adanalin113@gmail.com>
2022-05-25 22:54:58 -0400

    commit tree f57072f7f1496afe7129eb7ddc757661aed93c73

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your code in this file!
+const currentUser = 'Adan Alin';
+const welcomeMessage ='Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() ;
+const shortGreeting = "Welcome, " + currentUser[0] + '!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const currentUser = "Grace Hopper";
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!' ;
